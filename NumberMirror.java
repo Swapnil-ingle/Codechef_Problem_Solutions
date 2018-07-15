99160c472505ef11613a5a498f531937cf76e50c
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2018-07-15 22:39:13 +0530

    Added: Solution to 'NumberMirror' problem from codechef.com

---
+++ NumberMirror.java
@@ -0,0 +1,12 @@
+package com.coding.codechef.practice;
+
+import java.util.Scanner;
+
+class NumberMirror {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println(input.nextLong());
+		input.close();
+	}
+	
+}
